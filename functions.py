3857004ea27869e6d60e21c0e37e2ffccb9f0607
ywsyws <channing.platevoet@gmail.com>
2019-12-12 16:05:10 +0000

    add config function in db conn

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 from urllib import parse
+import pyodbc
 
 def parse_url(url):
     """ Parce URL into dictionary for INPUT to Azure SQL server
@@ -7,3 +8,17 @@ def parse_url(url):
     url_query = dict(parse.parse_qsl(parse.urlsplit(url).query))
     print (url_query)
     return url_query
+
+def sqldb_conn(server, db, uid, pwd):
+    """ Establish connection with the Azure SQL database
+    """
+
+    # Connect to the Azure SQL Database
+    conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
+                        'Server=azuresqlorange.database.windows.net;'
+                        'Database=orange_azure;'
+                        'Trusted_Connectoin=yes;'
+                        'UID=orange;'
+                        'PWD=Supermotdepasse!42;')
+    cursor = conn.cursor()
+    return conn, cursor
--- url2sql.py
+++ url2sql.py
@@ -1,44 +1,41 @@
 # import libraries
-import pyodbc
 import pandas as pd
-from functions import parse_url
+from functions import parse_url, sqldb_conn
+import config
 
 # Set URL
 url = 'https://www.yws.com?user_id=11&movie_id=111&comment=asdf'
 
 url_query = parse_url(url)
 
-# server = azuresqlorange.database.windows.net
-# db = orange_azure
-# uid = orange
-# pwd= Supermotdepasse!42
-# cursor = sqldb_connection(server, db, uid, pwd)
+server = config.cf['server']
+db = config.cf['db']
+uid = config.cf['uid']
+pwd= config.cf['pwd']
 
+conn, cursor = sqldb_conn(server, db, uid, pwd)
 
-
-def sqldb_connection(server, db, uid, pwd):
-
-    # Connect to the Azure SQL Database
-    conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
-                        'Server=azuresqlorange.database.windows.net;'
-                        'Database=orange_azure;'
-                        'Trusted_Connectoin=yes;'
-                        'UID=orange;'
-                        'PWD=Supermotdepasse!42;')
-
-    return conn.cursor()
-
-# Create a new record
-sql_insert = """
-            INSERT INTO cc_comments (user_id, movie_id, comment)
-            VALUES (?, ?, ?)
+query = """SELECT TOP 10 year, COUNT(year) AS count
+            FROM analysis_movies
+            GROUP BY year
+            ORDER BY count DESC
             """
 
-# Execute the query and write to Azure DB table
-cursor.execute(sql_insert, url_query['user_id'], url_query['movie_id'], url_query['comment'])
+MostBestMovieYear = pd.read_sql(query, conn)
+print(MostBestMovieYear)
 
-# Commit the connection since it is not autocimmited by default
-conn.commit()
 
-# Close the connection
-conn.close()
+# # Create a new record
+# sql_insert = """
+#             INSERT INTO cc_comments (user_id, movie_id, comment)
+#             VALUES (?, ?, ?)
+#             """
+
+# # Execute the query and write to Azure DB table
+# cursor.execute(sql_insert, url_query['user_id'], url_query['movie_id'], url_query['comment'])
+
+# # Commit the connection since it is not autocimmited by default
+# conn.commit()
+
+# # Close the connection
+# conn.close()
